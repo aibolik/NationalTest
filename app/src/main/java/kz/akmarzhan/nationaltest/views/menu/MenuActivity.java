9957256d6cadb361d60869fdf3b2762222e532bf
Aibol Kussain <aibolikdev@gmail.com>
2017-06-07 20:37:54 +0600

    feat: add logout button

--- app/src/main/java/kz/akmarzhan/nationaltest/views/menu/MenuActivity.java
+++ app/src/main/java/kz/akmarzhan/nationaltest/views/menu/MenuActivity.java
@@ -7,6 +7,9 @@ import android.support.annotation.Nullable;
 import android.support.design.widget.FloatingActionButton;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
@@ -75,6 +78,22 @@ public class MenuActivity extends BaseActivity implements PredmetsAdapter.Predme
 
         showDialog("", "Loading...");
         getBus().post(new LoadPredmetsEvent(mUser.getObjectId()));
+    }
+
+    @Override public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.logout:
+                logout();
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
     }
 
     @Subscribe
